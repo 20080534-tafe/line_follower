3d52b0ee16acf075aaba17ae0fab185512619eaa
20080534-tafe <20080534@tafe.wa.edu.au>
2025-11-19 11:17:57 +0800

    Update setup.py with data_files and an entry_point

--- setup.py
+++ setup.py
@@ -8,13 +8,15 @@ setup(
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+         ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['launch/line_follower_launch.py']),
+        ('share/' + package_name + '/config', ['config/line_follower_params.yaml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='webot',
-    maintainer_email='webot@todo.todo',
+    maintainer_email='20080534@tafe.wa.edu.au',
     description='TODO: Package description',
     license='TODO: License declaration',
     extras_require={
@@ -24,6 +26,7 @@ setup(
     },
     entry_points={
         'console_scripts': [
+            'avoid_wall = line_follower.line_follower_node:main'
         ],
     },
 )
